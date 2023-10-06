f6e448efded085a2709f4885149c1363a11563b2
Алексей Школьный <akademegex36512@gmail.com>
2023-10-06 20:50:46 +0300

    Добавлена функция приветствия на 6 языках с выбором языка пользователя.

---
+++ hello.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var Language := ReadString('Enter your language in English:');
+  case Language of 
+    'English': Println('Hello!');
+    'Russian': Println('Здравствуй!');
+    'Spanish': Println('Hola!');
+    'Italian': Println('Ciao!');
+    'German': Println('Servus!');
+    'Turkish': Println('Merhaba!');
+  end;
+end.
